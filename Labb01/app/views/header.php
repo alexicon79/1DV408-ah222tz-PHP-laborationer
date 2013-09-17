6e2acaae41cdfdb48d256b6140c984ec2b597478
Alexander Hall <ah@alxhall.se>
2013-09-17 11:18:33 +0200

    markup validation adjustments

--- Labb01/app/views/header.php
+++ Labb01/app/views/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>Labb 01</title>
-<meta http-equiv="Content-type" content="text/html" charset="utf-8" />
+	<title>1DV408 - Labb 01</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
