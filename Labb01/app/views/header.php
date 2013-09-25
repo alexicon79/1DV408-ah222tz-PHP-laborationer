0b03f4263c3b2a8192a3790f54f8103607634e85
Alexander Hall <ah@alxhall.se>
2013-09-25 18:30:59 +0200

    Original commit - code review

--- Labb01/app/views/header.php
+++ Labb01/app/views/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>1DV408 - Labb 01</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
